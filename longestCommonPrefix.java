b44182366f1216c602f967ab5a89ba153b404047
Raj <yadavrajkaran854@gmail.com>
2024-01-19 12:10:35 +0530

    Enhance solution for longestCommonPrefix - [Ref: 707a]

---
+++ longestCommonPrefix.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class longestCommonPrefix {
+    public String longestCommonPrefix(String[] s) {
+        if (s.length == 0) return "";
+        int j = 0;
+        outer: while (j < s[0].length()) {
+            char c = s[0].charAt(j);
+            for (int i = 1; i < s.length; i++) {
+                if (j >= s[i].length() || s[i].charAt(j) != c) break outer;
+            }
+            j++;
+        }
+        return s[0].substring(0, j);
+    }
+}
+
+
+
+
+ 
+ 
+ 
+ 
+ 
