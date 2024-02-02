8a6fc3a47b5f5437ea2a7845b05a9cfa2072a848
Raj <yadavrajkaran854@gmail.com>
2024-02-02 16:12:35 +0530

    Enhance solution for RangeMaxQuery - [Ref: 5c74]

---
+++ RangeMaxQuery.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class RangeMaxQuery {
+        static void build(int i, int l, int r, int[] seg, int[] arr) {
+            if (l == r) {
+                seg[i] = arr[l];
+                return;
+            }
+            int mid = (l+r)/2;
+            build(2*i+1, l, mid, seg, arr);
+            build(2*i+2, mid+1, r, seg, arr);
+            seg[i] = Math.max(seg[2*i+1], seg[2*i+2]);
+        }
+
+        static int query(int ql, int qr, int i, int l, int r, int[] seg) {
+            if (qr < l || ql > r) return Integer.MIN_VALUE;
+            if (ql <= l && r <= qr) return seg[i];
+
+            int mid = (l+r)/2;
+            return Math.max(
+                    query(ql, qr, 2*i+1, l, mid, seg),
+                    query(ql, qr, 2*i+2, mid+1, r, seg)
+            );
+        }
+
+        static void update(int idx, int val, int i, int l, int r, int[] seg) {
+            if (l == r) {
+                seg[i] = val;
+                return;
+            }
+            int mid = (l+r)/2;
+            if (idx <= mid) update(idx, val, 2*i+1, l, mid, seg);
+            else update(idx, val, 2*i+2, mid+1, r, seg);
+
+            seg[i] = Math.max(seg[2*i+1], seg[2*i+2]);
+        }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
+ 
