2135ddb1af7cddb07edd176979ed68bd7b2ced99
Raj <yadavrajkaran854@gmail.com>
2024-02-18 10:27:02 +0530

    Enhance solution for threeSum - [Ref: 2949]

---
+++ threeSum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class threeSum {
+    public List<List<Integer>> threeSum(int[] a) {
+        Set<List<Integer>> r = new HashSet<>();
+        Arrays.sort(a);
+        for (int i = 0; i < a.length - 2; i++) {
+            int l = i + 1, h = a.length - 1;
+            while (l < h) {
+                int s = a[i] + a[l] + a[h];
+                if (s == 0) {
+                    r.add(Arrays.asList(a[i], a[l], a[h]));
+                    l++; h--;
+                } else if (s < 0) l++;
+                else h--;
+            }
+        }
+        return new ArrayList<>(r);
+    }
+}
+
+
+
+
+ 
+ 
+ 
+ 
+ 
