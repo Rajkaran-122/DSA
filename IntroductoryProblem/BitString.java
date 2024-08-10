caee3638f6a57f3547496a620b3a5eef44e71de7
Raj <yadavrajkaran854@gmail.com>
2024-08-10 19:32:28 +0530

    Enhance solution for BitString - [Ref: 19b4]

---
+++ IntroductoryProblem/BitString.java
@@ -0,0 +1,28 @@
+package IntroductoryProblem;
+
+import java.util.*;
+import java.io.*;
+
+public class BitString {
+    public static int solve(int n){
+        long ans=1;
+        for(int i=0; i<n; i++){
+            ans=(ans*2)%1000000007;
+        }
+        return (int)ans%1000000007;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(solve(n));
+
+
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
