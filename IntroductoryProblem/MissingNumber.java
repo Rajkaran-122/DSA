6a010a463b215f13edaa45fadbf295edc5417894
Raj <yadavrajkaran854@gmail.com>
2024-08-16 15:26:06 +0530

    Enhance solution for MissingNumber - [Ref: 6699]

---
+++ IntroductoryProblem/MissingNumber.java
@@ -0,0 +1,25 @@
+package IntroductoryProblem;
+
+import java.util.*;
+import java.io.*;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  n =sc.nextInt();
+        long sum=0;
+        int arr[] = new int[n-1];
+        for(int i=0; i<n-1; i++){
+            arr[i]=sc.nextInt();
+            sum+=arr[i];
+        }
+        System.out.println((long)n*(n+1)/2-sum);
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
