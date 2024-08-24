33a69342e30ce6ab438ce90cb571642b6174eddf
Raj <yadavrajkaran854@gmail.com>
2024-08-24 14:41:39 +0530

    Enhance solution for WiredAlgorithum - [Ref: 7c65]

---
+++ IntroductoryProblem/WiredAlgorithum.java
@@ -0,0 +1,60 @@
+package IntroductoryProblem;
+
+import java.util.*;
+import java.io.*;
+
+public class WiredAlgorithum {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastReader sc = new FastReader();
+        long n = sc.nextInt();
+        System.out.print(n + " ");
+        while (n != 1&&n>0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            System.out.print(n + " ");
+        }
+        System.out.println();
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
