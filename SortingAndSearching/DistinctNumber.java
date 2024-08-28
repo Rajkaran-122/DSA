f2cc654d8f69ece73ea52febc4150a5bef4af573
Raj <yadavrajkaran854@gmail.com>
2024-08-28 18:34:58 +0530

    Enhance solution for DistinctNumber - [Ref: 2392]

---
+++ SortingAndSearching/DistinctNumber.java
@@ -0,0 +1,29 @@
+package SortingAndSearching;
+
+import java.util.*;
+import java.io.*;
+
+public class DistinctNumber {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        System.out.println(set.size());
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
