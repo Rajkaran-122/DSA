8e5adbf1be9878690d501a4526aef5530402304f
Raj <yadavrajkaran854@gmail.com>
2024-09-06 17:01:27 +0530

    Enhance solution for SubArraySum2 - [Ref: f273]

---
+++ SortingAndSearching/SubArraySum2.java
@@ -0,0 +1,55 @@
+package SortingAndSearching;
+
+import java.util.*;
+import java.io.*;
+
+//
+
+
+
+
+
+
+
+public class SubArraySum2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        long[] arr = new long[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Long.parseLong(st.nextToken());
+        }
+
+        HashMap<Long, Integer> map = new HashMap<>();
+        map.put(0L, 1);
+        long pre[] = new long[n];
+        pre[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] + arr[i];
+        }
+        long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            long val = pre[i] - x;
+            if (map.containsKey(val)) {
+                ans += map.get(val);
+            }
+            map.put(pre[i], map.getOrDefault(pre[i], 0) + 1);
+        }
+        System.out.println(ans);
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
