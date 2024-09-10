24b5eade62f4e9c293467038bcbf6cf274b1a0a7
Raj <yadavrajkaran854@gmail.com>
2024-09-10 11:33:01 +0530

    Enhance solution for 2sum - [Ref: 939f]

---
+++ 2sum.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+/*
+ * PUSH TO REPOSITORY COMMANDS:
+ * 
+ * Stage changes:
+ git add .
+ * 
+ * Commit changes:
+ git commit -m "Your commit message here"
+ * 
+ * Push to GitHub:
+ git push
+ * 
+ * Repository: https://github.com/Raj/DSA.git
+ */
+
+class 2sum {
+    public int[] twoSum(int[] nums, int target) {
+       Map<Integer,Integer> add = new HashMap<>();
+
+       for(int i =0 ; i< nums.length ;i++){
+        int comp = target - nums[i];
+         if(add.containsKey(comp)){
+            return new int[] {i,add.get(comp)};
+         }
+         add.put(nums[i],i);
+       }
+       return new int[]{};
+    }
+}
+
+
+
+ 
+ 
+ 
+ 
+ 
