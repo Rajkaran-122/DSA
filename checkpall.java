669fab4fc1b0b1a327772747394dff84d36edacd
Rajkaran <yadavrajkaran854@gmail.com>
2026-03-30 19:49:09 +0530

    Add isPalindrome method to check if an integer is a palindrome

---
+++ checkpall.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers or numbers ending with 0 (but not 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x = x / 10;
+        }
+
+        // If even length, x == reversedHalf
+        // If odd length, x == reversedHalf / 10
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+}
