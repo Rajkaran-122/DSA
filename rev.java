7d98d9caacdd9f62370191b0c40a9250c6b070e4
Rajkaran <yadavrajkaran854@gmail.com>
2026-03-30 19:44:38 +0530

    Add reverse method to reverse an integer with overflow checks

---
+++ rev.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int r = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+
+            // Check for overflow before actually multiplying or adding
+            if (r > Integer.MAX_VALUE / 10 || (r == Integer.MAX_VALUE / 10 && digit > 7)) return 0;
+            if (r < Integer.MIN_VALUE / 10 || (r == Integer.MIN_VALUE / 10 && digit < -8)) return 0;
+
+            r = r * 10 + digit;
+        }
+        return r;
+    }
+}
